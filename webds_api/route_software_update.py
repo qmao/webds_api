a877672e481f3e8eb4b2d6f364acaaba47cbaf12
root <root@pi-syna>
2022-07-06 21:27:41 -0700

    route_software_update add timestamp

--- webds_api/route_software_update.py
+++ webds_api/route_software_update.py
@@ -2,6 +2,7 @@ import tornado
 from tornado.iostream import StreamClosedError
 from jupyter_server.base.handlers import APIHandler
 import os
+import os.path
 import re
 import json
 
@@ -10,22 +11,31 @@ class UpdateMonitor(object):
     _filename = ""
     def __init__(self):
         self._stamp = 0
-        self.filename = "/var/log/syna/update_daemon.log"
+        self._filename = "/var/log/syna/update_daemon.log"
 
     def getStatus(self):
-        lstamp = os.stat(self.filename).st_mtime
-        if self._stamp == lstamp:
-            return
-        self._stamp = lstamp
-        print(self._stamp)
+        ret = dict();
+        ret['status'] = None
+        ret['timestamp'] = -1
 
-        for line in reversed(list(open(self.filename))):
-            print(line.rstrip())
-            pattern = '[@]\w+.(\w+)'
-            result = re.findall(pattern, line)
-            if len(result) != 0:
-                return result[0]
-        return None
+        if os.path.exists(self._filename):
+            lstamp = os.stat(self._filename).st_mtime
+            if self._stamp == lstamp:
+                return ret
+            self._stamp = lstamp
+
+            for line in reversed(list(open(self._filename))):
+                print(line.rstrip())
+                pattern = '[@]\w+.(\w+)'
+                result = re.findall(pattern, line)
+                if len(result) != 0:
+                    ret['status'] = result[0]
+                    pattern = '(\d+:\d+:\d+/\d+/\d+:\d+:\d+:\d+.\d+)'
+                    time = re.findall(pattern, line)
+                    if len(time) != 0:
+                        ret['timestamp'] = time[0]
+                    break
+        return ret
 
 class SoftwareUpdateHandler(APIHandler):
     @tornado.web.authenticated
@@ -50,10 +60,9 @@ class SoftwareUpdateHandler(APIHandler):
         try:
             monitor = UpdateMonitor()
             while True:
-               status = monitor.getStatus()
-               if status is not None:
-                   send = { "status": status }
-                   send = json.dumps(send)
+               ret = monitor.getStatus()
+               if ret['status'] is not None:
+                   send = json.dumps(ret)
                    yield self.publish(send)
                else:
                    yield tornado.gen.sleep(0.0001)
