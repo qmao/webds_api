595e9eab00c82d54f6560b808e71589e55c391e9
root <root@pi-syna>
2022-08-08 03:13:39 -0700

    add endpoint to reset rpi4 device

--- webds_api/route_general.py
+++ webds_api/route_general.py
@@ -3,12 +3,12 @@ from jupyter_server.base.handlers import APIHandler
 import os
 import json
 from .utils import SystemHandler
+from uuid import uuid4
 
+
+rand_token = str(uuid4())
 
 class GeneralHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post,
-    # patch, put, delete, options) to ensure only authorized user can request the
-    # Jupyter server
     @tornado.web.authenticated
     def get(self):
         print(self.request)
@@ -18,3 +18,42 @@ class GeneralHandler(APIHandler):
         self.finish(json.dumps({
             "data": "webds-api server is running"
         }))
+
+    @tornado.web.authenticated
+    def post(self):
+        data = self.get_json_body()
+        print(data)
+        response = {}
+
+        if "command" in data:
+            param = data["command"]
+            print(param)
+            if "action" in param and param["action"] == "reset":
+                print("reset")
+                if "target" in param and param["target"] == "rpi4":
+                    print("reset rpi4")
+                    global rand_token
+                    print(rand_token)
+                    if "uuid" in param:
+                        print("uuid found")
+                        uuid = param["uuid"]
+                        print(uuid)
+                        if uuid == rand_token:
+                            print("uuid matched")
+                            self.finish(json.dumps("done"))
+                            SystemHandler.CallSysCommandFulfil('reboot')
+                            return
+                        else:
+                            print("uuid not matched")
+                            raise tornado.web.HTTPError(status_code=400, log_message="uuid not matched")
+                    else:
+                        rand_token = str(uuid4())
+                        print(rand_token)
+                        self.finish(json.dumps(rand_token))
+                        return
+                else:
+                    print("target not set")
+        else:
+            print("command not set")
+
+        raise tornado.web.HTTPError(status_code=400, log_message="unsupported action")
