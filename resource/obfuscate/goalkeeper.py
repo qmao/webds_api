ee19e7ab7a54565ce3aa5f71c846e5eea38a523e
Alexandra Chin <alexandra.chin@tw.synaptics.com>
2022-09-06 03:42:53 -0700

    check stack before sending system commands

--- resource/obfuscate/goalkeeper.py
+++ resource/obfuscate/goalkeeper.py
@@ -1,8 +1,47 @@
 import os
 import subprocess
+import traceback
+import re
 
 class Goalkeeper():
+    def CheckStack():
+        slist = []
+        for line in traceback.format_stack():
+            current=line.strip()
+            regex = re.compile('(?<=File \")[A-Za-z0-9-_.\s\/]+(?=\",)')
+            found = regex.search(current)
+
+            if found is not None:
+                slist.append(found.group(0))
+
+        isCallFromJupyterLab = False
+        if "/usr/local/bin/jupyter-lab" in slist[0]:
+            isCallFromJupyterLab = True
+
+        slist.reverse()
+
+        isCallFromUtil = False
+        if slist[2] == "/usr/local/lib/python3.7/dist-packages/webds_api/utils.py":
+            isCallFromUtil = True
+
+        isCallFromRoute = False
+        regex = re.compile('^/usr/local/lib/python3.7/dist-packages/webds_api/route/')
+        found = regex.search(slist[3])
+
+        if found is not None:
+            isCallFromRoute = True
+
+        if isCallFromUtil and isCallFromRoute and isCallFromJupyterLab:
+            return True
+        else:
+            print("Invalid call stack")
+            for path in slist:
+                print(path)
+            return False
+
     def CallSysCommand(command, user = False):
+        if Goalkeeper.CheckStack() == False:
+            return
         if os.geteuid() != 0 and user != True:
             command = ' '.join(command)
             command = ['su', '-c', command]
@@ -11,6 +50,8 @@ class Goalkeeper():
         subprocess.run(command, input=password.stdout)
 
     def CallSysCommandCapture(command, user = False):
+        if Goalkeeper.CheckStack() == False:
+            return ""
         if os.geteuid() != 0 and user != True:
             command = ' '.join(command)
             command = ['su', '-c', command]
@@ -20,6 +61,8 @@ class Goalkeeper():
         return result.stdout
 
     def CallSysCommandFulfil(command, user = False):
+        if Goalkeeper.CheckStack() == False:
+            return
         if os.geteuid() != 0 and user != True:
             command =  "echo 'pi.x@=syna' | su -c " + "'" + command + "'"
 
