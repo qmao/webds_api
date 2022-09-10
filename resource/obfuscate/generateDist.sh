bf1e648f0cfa3d9ab6a3824ad7092e39c2091312
Alexandra Chin <alexandra.chin@tw.synaptics.com>
2022-09-10 09:49:48 -0700

    add missing file

--- resource/obfuscate/generateDist.sh
+++ resource/obfuscate/generateDist.sh
@@ -1,5 +1,3 @@
 /usr/local/bin/pyarmor obfuscate -r --bootstrap 2 --restrict=0 --platform linux.armv7 goalkeeper.py
-sed -i 's/from pytransform/from .pytransform.pytransform/g' dist/goalkeeper.py
 cp dist/goalkeeper.py ../../webds_api/obfuscate/goalkeeper.py
 cp dist/pytransform/__init__.py ../../webds_api/obfuscate/pytransform/pytransform.py
-cp dist/pytransform/_pytransform.so ../../webds_api/obfuscate/pytransform/_pytransform.so
