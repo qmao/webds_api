2b17ac5390c13f33e8b22773fb16e7b66bdfdc7b
Alexandra <alexandra.chin@tw.synaptics.com>
2023-01-09 00:30:32 -0800

    tutor: update naming convention

--- webds_api/tutor/max_capacitance/max_capacitance.py
+++ webds_api/tutor/max_capacitance/max_capacitance.py
@@ -3,13 +3,14 @@ import re
 import time
 import numpy as np
 
+
 class MaxCapacitance():
+    REPORT_ID = 18
     _handle = None
     _start = None
     _callback = None
     _terminate = False
     _terminated = False
-    _report_id = 18
     _max = -sys.maxsize - 1
     _cumMax = -sys.maxsize - 1
 
@@ -27,24 +28,24 @@ class MaxCapacitance():
         self._handle.disableReport(17)
         self._handle.disableReport(19)
 
-    def setReport(self, enable, report):
+    def set_report(self, enable, report):
         if enable:
             ret = self._handle.enableReport(report)
         else:
             ret = self._handle.disableReport(report)
         return True
 
-    def getReport(self):
+    def get_report(self):
         try:
             report = self._handle.getReport(0.5)
             if report[0] == 'delta':
                 return report[1]['image']
         except Exception as e:
-            print("[getReport error]", str(e))
+            print("[get_report error]", str(e))
             pass
         return None
 
-    def printTime(self, tag):
+    def print_time(self, tag):
         if False:
             if self._start == None:
                 self._start = time.time()
@@ -52,20 +53,20 @@ class MaxCapacitance():
             print("[ TIME ]", tag, "--- %s seconds ---" % (now - self._start))
             self._start = now
 
-    def updateInfo(self, data, state = "run"):
+    def update_info(self, data, state = "run"):
         self._callback({"state": state, "value": data})
 
     def run(self):
         try:
             self.init()
-            self.setReport(True, self._report_id)
+            self.set_report(True, self.REPORT_ID)
             self._terminated = self._terminate
             while self._terminate is False:
-                report = self.getReport()
+                report = self.get_report()
                 if report is not None:
                     self._max = np.amax(report)
                     self._cumMax = max(self._max, self._cumMax)
-                    self.updateInfo({"max": int(self._max), "cum_max": int(self._cumMax)}, "run")
+                    self.update_info({"max": int(self._max), "cum_max": int(self._cumMax)}, "run")
 
             print("run While loop terminate")
             self._terminated = True
