7f55ecfa40d4782e55137dfb78c05a88cf16c6c2
root <root@pi-syna>
2023-01-16 22:08:58 -0800

    tutor: update int_dur backend

--- webds_api/tutor/int_dur/int_dur_route.py
+++ webds_api/tutor/int_dur/int_dur_route.py
@@ -1,35 +1,70 @@
-import numpy as np
-from scipy.optimize import curve_fit
+import inspect
+import math
+import threading
+import time
 
-from .integration_duration import IntegrationDuration
+from ...touchcomm.touchcomm_manager import TouchcommManager
+from ..tutor_utils import EventQueue
+from .int_dur import IntDurTuner
 
-def sigmoid(x, L ,x0, k, b):
-    y = L / (1 + np.exp(-k*(x-x0))) + b
-    return y
 
 class IntDurRoute():
+    _tutor = None
+    _cancel = False
+
+    def _run_generator_thread(tutor_function, args):
+        try:
+            if args is None:
+                generator = tutor_function()
+            else:
+                generator = tutor_function(*args)
+
+            for progress in generator:
+                if IntDurRoute._cancel:
+                    EventQueue().push({"state": "terminated"})
+                    EventQueue().close()
+                    return
+                else:
+                    progress = math.floor(progress * 100)
+                    EventQueue().push({"state": "running", "progress": progress})
+
+            time.sleep(0.5)
+            EventQueue().push({"state": "completed"})
+        except:
+            EventQueue().push({"state": "terminated"})
+        finally:
+            EventQueue().close()
+
     def post(handle, input_data):
         request = input_data["request"]
         args = None
         if "arguments" in input_data:
             args = input_data["arguments"]
 
-        idm = IntegrationDuration()
+        if IntDurRoute._tutor is None:
+            tc = TouchcommManager().getInstance()
+            IntDurRoute._tutor = IntDurTuner(tc)
 
         if request == None:
             raise Exception("No request specified: ", input_data)
 
-        if request == "sigmoid":
-            xdata = input_data["sigmoid"]["xdata"]
-            ydata = input_data["sigmoid"]["ydata"]
-            sigma = np.ones(len(xdata))
-            sigma[[0]] = 0.1
-            p0 = [max(ydata), np.median(xdata), 1, min(ydata)]
-            popt, pcov = curve_fit(sigmoid, xdata, ydata, p0, method='lm', maxfev=5000, sigma=sigma)
-            return popt.tolist()
+        if request == "cancel":
+            IntDurRoute._cancel = True
+            return None
 
+        retval = None
         try:
-            retval = idm.function(request, args)
-            return retval
+            tutor_function = getattr(IntDurRoute._tutor, request)
+            if inspect.isgeneratorfunction(tutor_function):
+                IntDurRoute._cancel = False
+                thread = threading.Thread(target=IntDurRoute._run_generator_thread, args=(tutor_function, args))
+                thread.start()
+            else:
+                if args is None:
+                    retval = tutor_function()
+                else:
+                    retval = tutor_function(*args)
         except Exception as e:
             raise Exception("Error executing {}: ".format(request), str(e))
+
+        return retval
