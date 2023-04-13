87fcc0f7833b0fc9dd71c6d697890257b8486c93
root <root@pi-syna>
2023-04-13 09:59:03 -0700

    tutor: sensor_mapping: stub model

--- webds_api/tutor/sensor_mapping/sensor_mapping.py
+++ webds_api/tutor/sensor_mapping/sensor_mapping.py
@@ -1,26 +1,6 @@
-import sys
-import re
-import time
-import json
-
 
 class SensorMapping():
-    _handle = None
-    _start = None
-    _debug = False
-    _terminate = False
-    _terminated = False
-    _static_config = {}
-    _dynamic_config = {}
-
     def __init__(self, handle):
-        self._handle = handle
-        self._static_config = self._handle.getStaticConfig()
-        self._dynamic_config = self._handle.getDynamicConfig()
-        self._touch_info = self._handle.getTouchInfo()
-        self._app_info = self._handle.getAppInfo()
-
-        self._terminate = False
-        self._terminated = False
+        print("init")
 
            
